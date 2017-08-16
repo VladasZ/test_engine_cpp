05289fc5cdf3189f59e43166f12d8a3667bb8419
Vladas Zakrevskis <146100@gmail.com>
2017-08-17 00:33:31 +0300

    initialized gl

--- Platforms/Windows/TestEngine/TestEngine/main.cpp
+++ Platforms/Windows/TestEngine/TestEngine/main.cpp
@@ -1,7 +1,11 @@
+#define GLEW_STATIC
+
 #include <iostream>
 #include <Windows.h>
 #include <Tools\Tools.hpp>
+#include "glew.h"
 #include "glfw3.h"
+
 
 using namespace std;
 
@@ -9,10 +13,78 @@ int main() {
 
 	Info("rglooooorglak!");
 
-
 	glfwInit();
-	glfwCreateWindow(100, 100, "Elite Engine", NULL, NULL);
 
-	Sleep(100000);
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+
+	auto window = glfwCreateWindow(1000, 1000, "Elite Engine", NULL, NULL);
+
+	if (window == nullptr) {
+
+		Error("kriglisalko");
+	}
+
+	glfwMakeContextCurrent(window);
+
+
+	glewExperimental = true;
+
+	auto err = glewInit();
+
+	cout << err << endl;
+
+
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	//glBindVertexArray(VertexArrayID);
+
+	// An array of 3 vectors which represents 3 vertices
+	static const GLfloat g_vertex_buffer_data[] =
+	{
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	for (;;) {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		Sleep(100);
+	}
+
+
+	Sleep(1000000);
 	return 0;
 }

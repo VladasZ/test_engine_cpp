4eeba964ad4bed7c5c317a2e090c98ba67111cd2
Vladas Zakrevskis <146100@gmail.com>
2017-08-16 23:22:47 +0300

    added glfw for windows

--- Platforms/Windows/TestEngine/TestEngine/main.cpp
+++ Platforms/Windows/TestEngine/TestEngine/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <Windows.h>
-#include <Tools/Tools.hpp>
+#include <Tools\Tools.hpp>
+#include "glfw3.h"
 
 using namespace std;
 
 int main() {
 
 	Info("rglooooorglak!");
+
+
+	glfwInit();
+	glfwCreateWindow(100, 100, "Elite Engine", NULL, NULL);
+
 	Sleep(100000);
 	return 0;
 }

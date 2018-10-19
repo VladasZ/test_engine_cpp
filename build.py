db16bc565715f86b89717438fd8f0bc3fcf4ed9e
Vladas Zakrevskis <146100@gmail.com>
2018-10-19 15:04:17 +0300

    removed utils submodule, new build test

--- build.py
+++ build.py
@@ -1,30 +1,19 @@
 import os
+import shutil
+
+def rm(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+import git
+
+
+if not os.path.exists("utils"):
+        git.Git(".").clone("https://github.com/vladasz/utils")
+
 import sys
-sys.path.append('config/utils')
-import File
+sys.path.append('utils')
 import Args
-import Conan
-import Cmake
-import Shell
-import System
-import Android
+import Build
 
-Cmake.setup()
-Conan.setup()
-
-build_folder = File.build_folder()
-
-if Args.rmbuild:
-	File.rm(build_folder)
-
-if Args.android:
-	Android.setup()
-	
-File.mkdir(build_folder)
-File.cd(build_folder)
-
-Conan.run()
-Cmake.run()
-
-if Args.make:
-	Shell.run(['make'])
+# Build.cpp()

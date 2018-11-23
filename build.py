b6ba256140d4430e59bc7bf4abc39a9e2fff4148
Vladas Zakrevskis <146100@gmail.com>
2018-11-23 15:28:25 +0300

    test

--- build.py
+++ build.py
@@ -5,12 +5,6 @@ def rm(path):
     if os.path.exists(path):
         shutil.rmtree(path)
 
-import git
-
-
-if not os.path.exists("utils"):
-        git.Git(".").clone("https://github.com/vladasz/utils")
-
 import sys
 sys.path.append('utils')
 import Args

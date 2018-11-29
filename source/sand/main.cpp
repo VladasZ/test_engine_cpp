abc1834a178a34626969c626d613f7081e67c775
Vladas Zakrevskis <146100@gmail.com>
2018-11-29 10:43:44 +0300

    bitwise::set_byte fix

--- source/sand/main.cpp
+++ source/sand/main.cpp
@@ -1,12 +1,26 @@
 
 #include <iostream>
 
-#include "lib.hpp"
-#include "Singletone.hpp"
+#include "Bitwise.hpp"
 
 using namespace std;
 
+struct Test {
+
+	uint64_t num = 3;
+};
+
 int main() {
+
+	Test t;
+
+
+	cout << bitwise::to_number(t) << endl;
+
+	auto t2 =		bitwise::set_byte(t, 2, 1);
+
+	cout << bitwise::to_number(t2) << endl;
+
 
   cout << "Test OK" <<endl;
 

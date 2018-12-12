cecfec3ab97bd33814c23e2023cccb37e494f7b6
Vladas Zakrevskis <146100@gmail.com>
2018-12-12 16:11:20 +0300

    boost python test

--- source/sand/main.cpp
+++ source/sand/main.cpp
@@ -5,14 +5,17 @@
 #include <vector>
 #include <math.h>
 
+#include <Eigen/Dense>
+
 #include "Log.hpp"
 #include "Matrix4.hpp"
 
-using namespace std;
+#include "python_test.hpp"
 
-//constexpr 
-float delay_to_angle(float delay) {
-	Logvar(delay);
+using namespace std;
+using namespace Eigen;
+
+constexpr float delay_to_angle(float delay) {
 	return static_cast<float>(M_PI) * (delay - 4000) / 8333;
 }
 
@@ -29,20 +32,12 @@ vector<vector<float>> delays = {
   { 3807, 2291 },
   { 3742, 2176 }
 };
- 
-//plane1 = [+sympy.cos(a1), 0, -sympy.sin(a1)]
-//# Normal to Y plane
-//plane2 = [0, +sympy.cos(a2), +sympy.sin(a2)]
 
 Vector3 ray(float angle1, float angle2) {
-	Logvar(angle1);
-	Logvar(angle2);
-	Vector3 plane1 = { cos(angle1), 0,           -sin(angle2) };
-	Vector3 plane2 = { 0,           cos(angle2),  sin(angle2) };
-	return plane1.cross(plane2).normalize();
+	Vector3 plane1 = { cos(angle1),           0, -sin(angle1) };
+	Vector3 plane2 = {           0, cos(angle2),  sin(angle2) };
+	return plane2.cross(plane1).normalize();
 }
-
-
 
 
 int main() {
@@ -54,23 +49,43 @@ int main() {
   auto r13 = (sensors_position[1] - sensors_position[3]).length();
   auto r23 = (sensors_position[2] - sensors_position[3]).length();
 
-  
   auto v0 = ray(delay_to_angle(delays[0][0]), delay_to_angle(delays[0][1]));
   auto v1 = ray(delay_to_angle(delays[1][0]), delay_to_angle(delays[1][1]));
   auto v2 = ray(delay_to_angle(delays[2][0]), delay_to_angle(delays[2][1]));
   auto v3 = ray(delay_to_angle(delays[3][0]), delay_to_angle(delays[3][1]));
 
-
   Logvar(v0.to_string());
   Logvar(v1.to_string());
   Logvar(v2.to_string());
   Logvar(v3.to_string());
-  
-  //v0 = ray(tick2angle(samples[0][id * 2]), tick2angle(samples[0][id * 2 + 1]))
-  // v1 = ray(tick2angle(samples[1][id * 2]), tick2angle(samples[1][id * 2 + 1]))
-  // v2 = ray(tick2angle(samples[2][id * 2]), tick2angle(samples[2][id * 2 + 1]))
-  // v3 = ray(tick2angle(samples[3][id * 2]), tick2angle(samples[3][id * 2 + 1]))
 
+  auto v01 = v0.dot(v1);
+  auto v02 = v0.dot(v2);
+  auto v03 = v0.dot(v3);
+  auto v12 = v1.dot(v2);
+  auto v13 = v1.dot(v3);
+  auto v23 = v2.dot(v3);
+  
+  Logvar(acos(v01) * 180 / M_PI);
+  Logvar(acos(v02) * 180 / M_PI);
+  Logvar(acos(v03) * 180 / M_PI);
+  Logvar(acos(v12) * 180 / M_PI);
+  Logvar(acos(v13) * 180 / M_PI);
+  Logvar(acos(v23) * 180 / M_PI);
+
+
+  Matrix3d A;
+  Vector3d b;
+  A << 1, 2, 3, 
+	   4, 5, 6, 
+	   7, 8, 10;
+  b << 3, 3, 4;
+  cout << "Here is the matrix A:\n" << A << endl;
+  cout << "Here is the vector b:\n" << b << endl;
+  Vector3d x = A.colPivHouseholderQr().solve(b);
+  cout << "The solution is:\n" << x << endl;
+
+  python_test();
   
   return 0;
 }

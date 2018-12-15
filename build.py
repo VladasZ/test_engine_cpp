885073f6370d27574e32c91027495b0711f19502
Vladas Zakrevskis <146100@gmail.com>
2018-12-15 12:46:12 +0300

    refactored build system location search

--- build.py
+++ build.py
@@ -1,13 +1,3 @@
-import os
-import shutil
-
-def rm(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-
 import sys
-sys.path.append('utils')
-import Args
+sys.path.append('build_tools')
 import Build
-
-# Build.cpp()

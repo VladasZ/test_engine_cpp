53d9fd608e21c3778696758a9bfc2d192b59fbb4
Vladas <146100@gmail.com>
2019-07-02 14:22:04 +0300

    ios build fix

--- source/test_engine_test/main.cpp
+++ source/test_engine_test/main.cpp
@@ -1,11 +1,27 @@
 
 #ifndef IOS_BUILD
 
+#include "Log.hpp"
 #include "TestView.hpp"
 #include "TestScene.hpp"
 #include "TestEngine.hpp"
 
+#include "Utils.hpp"
+
 int main() {
+
+
+    const char* spes;
+
+    using StringLiteral = decltype ("spes");
+
+    Info((cu::is_string<StringLiteral>));
+
+
+    Info(cu::type_name<cu::ArrayType<StringLiteral>>);
+
+    return 0;
+
     TestEngine::start_with_scene<TestScene>();
     return 0;
 }

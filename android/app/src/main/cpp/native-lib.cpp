d028ee68d2141cb57495cbb334d7963290d93022
Vladas <146100@gmail.com>
2019-12-06 18:19:58 +0300

    Asset Manager test

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -4,20 +4,28 @@
 #include "Screen.hpp"
 #include "System.hpp"
 #include "Vector3.hpp"
+#include "File.hpp"
+#include "Paths.hpp"
 
 using namespace cu;
 using namespace gm;
+using namespace te;
 
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_test_1engine_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++\n";
 
     Vector3 vec3 = { 1, 2, 3 };
 
-    auto _screen = new te::Screen({ 100, 100 });
+    hello += Paths::Shaders::ui() + "\n";
+    hello += Paths::Shaders::ui() / "ko" / "sopok" / "\n";
+
+    auto file = File("ic_launcher_background.xml");
+
+    hello += file.size();
 
     hello += System::user_name();
     hello += vec3.to_string();
--- android/app/src/main/java/com/example/test_engine/MainActivity.java
+++ android/app/src/main/java/com/example/test_engine/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.test_engine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import android.content.res.AssetManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,19 +15,24 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private AssetManager asset_manager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String dataPath = getFilesDir().getPath() + "/";
+        asset_manager = getResources().getAssets();
+
+        Log.d("TestEngine", dataPath);
+        Log.d("TestEngine", "lalalal");
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
+
     public native String stringFromJNI();
 }

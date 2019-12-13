8c75c435435948d2ec61a71c412eca4af37d767f
Vladas <146100@gmail.com>
2019-12-13 17:46:04 +0300

    gles2 testing

--- source/test_engine/tests/GLTest.cpp
+++ source/test_engine/tests/GLTest.cpp
@@ -8,9 +8,9 @@
 
 #include "GLTest.hpp"
 #include "Assets.hpp"
+#include "GLDebug.hpp"
 #include "GLWrapper.hpp"
 #include "OpenGLHeaders.hpp"
-#include "GLDebug.hpp"
 
 using namespace te;
 
@@ -33,14 +33,14 @@ void Test::drawTriangle() {
 //    GL(glEnableVertexAttribArray(0));
 //    GL(glDrawArrays(GL_TRIANGLES, 0, 3));
 
-    GL(glBegin(GL_QUADS)); // Start drawing a quad primitive
-
-    GL((-1.0f, -1.0f, 0.0f)); // The bottom left corner
-    GL(glVertex3f(-1.0f, 1.0f, 0.0f)); // The top left corner
-    GL(glVertex3f(1.0f, 1.0f, 0.0f)); // The top right corner
-    GL(glVertex3f(1.0f, -1.0f, 0.0f)); // The bottom right corner
-
-    GL(glEnd());
+//    GL(glBegin(GL_QUADS)); // Start drawing a quad primitive
+//
+//    GL((-1.0f, -1.0f, 0.0f)); // The bottom left corner
+//    GL(glVertex3f(-1.0f, 1.0f, 0.0f)); // The top left corner
+//    GL(glVertex3f(1.0f, 1.0f, 0.0f)); // The top right corner
+//    GL(glVertex3f(1.0f, -1.0f, 0.0f)); // The bottom right corner
+//
+//    GL(glEnd());
 
 }
 

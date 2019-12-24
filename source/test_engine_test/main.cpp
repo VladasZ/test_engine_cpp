a3bf8c7df5ac96452fcaac327bac47059df62a5c
Vladas <146100@gmail.com>
2019-12-24 18:23:33 +0300

    Class info tests

--- source/test_engine_test/main.cpp
+++ source/test_engine_test/main.cpp
@@ -20,6 +20,9 @@ public:
 
 class Dog {
 public:
+
+    int height;
+
     static constexpr std::string_view class_name() {
         return "Dog";
     }
@@ -38,57 +41,25 @@ public:
 
 constexpr auto cats_age = mapping::Property("age", &Cat::height);
 
-auto lala = decltype(cats_age)::is_property;
-
 constexpr auto CatsInfo = new_mapping::ClassInfo(
+        "Cat",
         std::make_tuple(
-                cats_age,
-                cats_age
+                mapping::Property("age",    &Cat::age   ),
+                mapping::Property("height", &Cat::height)
                 ));
 
-
+constexpr auto DogsInfo = new_mapping::ClassInfo(
+        "Dog",
+        std::make_tuple(
+                mapping::Property("height", &Dog::height)
+        ));
 
 //constexpr auto gauno = new_mapping::ClassInfo(333);
 
-
-constexpr std::string_view spes = cats_age.name;
-
-class Spes {
-
-};
-
-constexpr auto classes_info = std::make_tuple(ClassInfoLa<Cat>(), ClassInfoLa<Dog>());
-
-template <class Class, class Check>
-constexpr void check(bool& value, ClassInfoLa<Check> info) {
-    if constexpr (std::is_same_v<Check, Class>) {
-        value = true;
-    }
-}
-
-template <class T>
-constexpr bool exists() {
-    bool result = false;
-    std::apply([&](auto&&... args) {((
-            check<T>(result, args)
-    ), ...);}, classes_info);
-    return result;
-}
-
-
 int main() {
 
-    auto info = ClassInfoLa<Cat>();
+    Log(CatsInfo.to_string());
 
-    static_assert(exists<Dog>());
-
-    Log(info.name());
-
-    Log(exists<Cat>());
-    Log(exists<Dog>());
-    Log(exists<Spes>());
-
-    Log(std::string() + std::string(Cat::class_name()));
 
     return 0;
 }

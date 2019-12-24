1651b95ce60a757ea63fc8a7cfecdf909757f6cb
Vladas <146100@gmail.com>
2019-12-24 18:57:58 +0300

    NewMapper check test

--- source/test_engine_test/main.cpp
+++ source/test_engine_test/main.cpp
@@ -4,8 +4,7 @@
 #include <tuple>
 
 #include "Log.hpp"
-#include "Property.hpp"
-#include "ClassInfo.hpp"
+#include "NewMapper.hpp"
 #include "IterateTuple.hpp"
 #include "TestEngineTest.hpp"
 
@@ -41,25 +40,31 @@ public:
 
 constexpr auto cats_age = mapping::Property("age", &Cat::height);
 
-constexpr auto CatsInfo = new_mapping::ClassInfo(
+constexpr auto CatsInfo = mapping::ClassInfo(
         "Cat",
         std::make_tuple(
                 mapping::Property("age",    &Cat::age   ),
                 mapping::Property("height", &Cat::height)
                 ));
 
-constexpr auto DogsInfo = new_mapping::ClassInfo(
+constexpr auto DogsInfo = mapping::ClassInfo(
         "Dog",
         std::make_tuple(
                 mapping::Property("height", &Dog::height)
         ));
 
-//constexpr auto gauno = new_mapping::ClassInfo(333);
+
+constexpr auto mapper = mapping::Mapper(std::make_tuple(CatsInfo, DogsInfo));
 
 int main() {
 
     Log(CatsInfo.to_string());
+    Log(mapper.to_string());
 
+
+    if constexpr (mapper.exists<Cat>()) {
+        
+    }
 
     return 0;
 }

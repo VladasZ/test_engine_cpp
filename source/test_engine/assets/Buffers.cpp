91f5472a587312e8fd9a6d84724e370a7d76bf0f
Vladas <146100@gmail.com>
2020-05-04 07:07:16 +0300

    BufferData with ArrayView

--- source/test_engine/assets/Buffers.cpp
+++ source/test_engine/assets/Buffers.cpp
@@ -7,22 +7,51 @@
 //
 
 #include "Rect.hpp"
+#include "Vertex.hpp"
 #include "Buffers.hpp"
 #include "GLWrapper.hpp"
 #include "BufferData.hpp"
 #include "BufferConfiguration.hpp"
 
 using namespace gm;
+using namespace gl;
+using namespace std;
 
-static const Rect fulscreen_rect = { -1, -1, 2, 2 };
+static constexpr Rect rect = { -1, -1, 2, 2 };
+static Vertex::Indices rect_indices = { 0, 1, 3, 2 };
+static Vertex::Indices indices      = { 0, 1, 2, 3 };
+
 
 Buffers::Buffers() {
-    fullscreen = new gl::Buffer(gl::BufferData::from_rect(fulscreen_rect), gl::BufferConfiguration::_2);
-    fullscreen->draw_mode = GL::DrawMode::TriangleStrip;
 
-    fullscreen_image = new gl::Buffer(gl::BufferData::from_rect_to_image(fulscreen_rect), gl::BufferConfiguration::_2_2);
+    static const vector<Float> fullscreen_vertices = {
+            rect.origin.x,                   rect.origin.y,
+            rect.origin.x,                   rect.size.height + rect.origin.y,
+            rect.size.width + rect.origin.x, rect.size.height + rect.origin.y,
+            rect.size.width + rect.origin.x, rect.origin.y
+    };
+
+    fullscreen = new gl::Buffer(new BufferData(fullscreen_vertices, rect_indices), gl::BufferConfiguration::_2);
+    fullscreen->draw_mode = GL::DrawMode::TriangleStrip;
+    
+    static const vector<Float> image_vertices = {
+            rect.origin.x,                   rect.origin.y,                    0.0f,  1.0f, //|- |
+            rect.origin.x,                   rect.size.height + rect.origin.y, 0.0f,  0.0f, //|_ |
+            rect.size.width + rect.origin.x, rect.size.height + rect.origin.y, 1.0f,  0.0f, //| _|
+            rect.size.width + rect.origin.x, rect.origin.y,                    1.0f,  1.0f  //| -|
+    };
+
+    fullscreen_image = new gl::Buffer(new BufferData(image_vertices, rect_indices), gl::BufferConfiguration::_2_2);
     fullscreen_image->draw_mode = GL::DrawMode::TriangleStrip;
 
-    fullscreen_outline = new gl::Buffer(gl::BufferData::from_rect_to_rectangle(fulscreen_rect), gl::BufferConfiguration::_2);
+    static const std::vector<Float> outline_vertices = {
+            rect.origin.x,                   rect.origin.y,
+            rect.origin.x,                   rect.size.height + rect.origin.y,
+            rect.size.width + rect.origin.x, rect.size.height + rect.origin.y,
+            rect.size.width + rect.origin.x, rect.origin.y
+    };
+
+    fullscreen_outline = new gl::Buffer(new BufferData(outline_vertices, indices), gl::BufferConfiguration::_2);
     fullscreen_outline->draw_mode = GL::DrawMode::LineLoop;
+
 }

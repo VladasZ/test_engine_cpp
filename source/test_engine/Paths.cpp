138103b341ad74a3dbf915eb157d916d79fa9cdd
Vladas Zakrevskis <146100@gmail.com>
2020-11-20 18:11:08 +0300

    const paths

--- source/test_engine/Paths.cpp
+++ source/test_engine/Paths.cpp
@@ -11,7 +11,7 @@
 #include "Log.hpp"
 #include "Paths.hpp"
 #include "System.hpp"
-#include "Platform.hpp"
+#include "Target.hpp"
 #include "SystemInfo.hpp"
 
 #ifdef IOS_BUILD
@@ -22,7 +22,7 @@ using namespace cu;
 using namespace te;
 
 
-Path Paths::root = [] {
+const Path Paths::root = [] {
 #ifdef IOS_BUILD
     return Path() / obj_c::work_directory_path();
 #elif ANDROID_BUILD
@@ -32,20 +32,20 @@ Path Paths::root = [] {
 #endif
 }();
 
-Path Paths::assets = SystemInfo::is_android ?  "" : (root / "Assets");
+const Path Paths::assets = SystemInfo::is_android ?  "" : (root / "Assets");
 
-Path Paths::images = assets / "Images";
-Path Paths::models = assets / "Models";
-Path Paths::fonts  = assets / "Fonts";
-Path Paths::downloads = System::home() / "Downloads";
+const Path Paths::images = assets / "Images";
+const Path Paths::models = assets / "Models";
+const Path Paths::fonts  = assets / "Fonts";
+const Path Paths::downloads = System::home() / "Downloads";
 
-Path Paths::Shaders::root = assets / "Shaders";
+const Path Paths::Shaders::root = assets / "Shaders";
 
-Path Paths::Shaders::ui        = root / "ui";
-Path Paths::Shaders::sprites   = root / "sprites";
-Path Paths::Shaders::isometric = root / "isometric";
-Path Paths::Shaders::include   = root / "include";
-Path Paths::Shaders::test      = root / "test";
+const Path Paths::Shaders::ui        = root / "ui";
+const Path Paths::Shaders::sprites   = root / "sprites";
+const Path Paths::Shaders::isometric = root / "isometric";
+const Path Paths::Shaders::include   = root / "include";
+const Path Paths::Shaders::test      = root / "test";
 
 void Paths::dump() {
 

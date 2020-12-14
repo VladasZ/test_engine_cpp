89d94cd85efaed111be9902ade9bedd2ff5e422e
Vladas Zakrevskis <146100@gmail.com>
2020-12-14 21:00:52 +0300

    sprites selection debugging

--- source/test_engine/sprites/TeLevel.cpp
+++ source/test_engine/sprites/TeLevel.cpp
@@ -85,16 +85,21 @@ void TeLevel::setup_controls() {
 
         if (!touch->is_began()) return;
 
+        add_box(touch->position);
+
         if (selected_sprite) {
             selected_sprite->is_selected = false;
             selected_sprite = nullptr;
         }
 
+        Log << touch->position;
+
         for (auto sprite : _sprites) {
-            if (sprite->contains(touch->position)) {
-                sprite->is_selected = true;
-                selected_sprite = sprite;
-            }
+            if (!sprite->contains(touch->position)) continue;
+            sprite->is_selected = true;
+            selected_sprite = sprite;
+            Log << sprite;
+            break;
         }
 
     };

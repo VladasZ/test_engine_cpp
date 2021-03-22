54d66087998a5ec373146df495b99c38da5e9b84
Vladas Zakrevskis <146100@gmail.com>
2021-03-22 05:26:39 +0300

    constexpr buffers

--- source/test_engine/assets/Buffers.cpp
+++ source/test_engine/assets/Buffers.cpp
@@ -18,13 +18,13 @@ using namespace gl;
 using namespace std;
 
 static constexpr Rect rect = { -1, -1, 2, 2 };
-static Vertex::Indices rect_indices = { 0, 1, 3, 2 };
-static Vertex::Indices indices      = { 0, 1, 2, 3 };
+static constexpr array<Vertex::Index, 4> rect_indices = { 0, 1, 3, 2 };
+static constexpr array<Vertex::Index, 4> indices      = { 0, 1, 2, 3 };
 
 
 Buffers::Buffers() {
 
-    static const vector<float> fullscreen_vertices = {
+    static constexpr array<float, 8> fullscreen_vertices = {
             rect.origin.x,                   rect.origin.y,
             rect.origin.x,                   rect.size.height + rect.origin.y,
             rect.size.width + rect.origin.x, rect.size.height + rect.origin.y,
@@ -34,7 +34,7 @@ Buffers::Buffers() {
     fullscreen = new gl::Buffer(gl::BufferConfiguration::_2, fullscreen_vertices, rect_indices);
     fullscreen->draw_mode = GL::DrawMode::TriangleStrip;
 
-    static const vector<float> image_vertices = {
+    static constexpr array<float, 16> image_vertices = {
             rect.origin.x,                   rect.origin.y,                    0.0f,  1.0f, //|- |
             rect.origin.x,                   rect.size.height + rect.origin.y, 0.0f,  0.0f, //|_ |
             rect.size.width + rect.origin.x, rect.size.height + rect.origin.y, 1.0f,  0.0f, //| _|
@@ -44,7 +44,7 @@ Buffers::Buffers() {
     fullscreen_image = new gl::Buffer(gl::BufferConfiguration::_2_2, image_vertices, rect_indices);
     fullscreen_image->draw_mode = GL::DrawMode::TriangleStrip;
 
-    static const std::vector<float> outline_vertices = {
+    static constexpr array<float, 8> outline_vertices = {
             rect.origin.x,                   rect.origin.y,
             rect.origin.x,                   rect.size.height + rect.origin.y,
             rect.size.width + rect.origin.x, rect.size.height + rect.origin.y,

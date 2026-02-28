526a17d3ec4b6222ed0d8bdce55ff45c989e52d4
Vladas Zakrevskis <146100@gmail.com>
2026-02-28 03:20:55 +0200

    fixed some ui includes

--- source/ui/xmake.lua
+++ source/ui/xmake.lua
@@ -1,7 +1,18 @@
 target("ui_lib")
     set_kind("static")
     -- add_packages("rapidfuzz", "glaze", "cli11")
-    add_includedirs("./**")
-    add_includedirs("include", { public = true })
+    add_includedirs(
+        ".",
+        "./input",
+        "./basic/text",
+        "../cpp_utils/",
+        "../cpp_utils/logs",
+        "../cpp_utils/system",
+        "../cpp_utils/stl",
+        "../cpp_utils/meta",
+        "../cpp_utils/filesystem",
+        "../cpp_utils/misc"
+    )
+    add_deps("cpp_utils_lib")
     add_files("**.cpp|Main.cpp")
     add_headerfiles("**.hpp")
